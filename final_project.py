6e5b1a99cbed7a63120ed04e5fcaf9a62aeb2d50
Yu-An Ko <handcuffanndo@gmail.com>
2017-12-15 02:38:36 +0800

    add code to find the most common word among the posts

--- final_project.py
+++ final_project.py
@@ -4,16 +4,21 @@ import json
 
 # ===== Facebook =====
 # PART 1. DATA REQUEST
+# a function to get data (posts on my wall) from the Facebook API
 def request_my_facebook_data():
+    # set up the access token
     facebook_access_token = "EAACEdEose0cBAB47WTQRZBH5v0JrZBI7bu5W2Rx5RIfd14FoiwaNAXYFGZC0oix1YtqTYcNgjrxvOOBldhz6hceAHmWFZAPBmjqyryYF9HMn0j2lba0cNZBqYxBgyWYCRKtH23nfv8mZAzSdVy6M4ay6GdPBtZCZAcPGrXB2ThUxcEbZB0GGyI5dBw92ldDuZCamIZD"
 
+    # set up the base URL
     base_url = 'https://graph.facebook.com/me/feed'
 
+    # set up a dictionary of parameters
     params_diction = {}
     params_diction["access_token"] = facebook_access_token
     params_diction["limit"] = 50
     params_diction["fields"] = "message, link, name, comments, likes"
 
+    # request data
     results = requests.get(url = base_url, params = params_diction)
     facebook_data_py = json.loads(results.text)
 
@@ -55,18 +60,43 @@ class Post:
     def __str__(self):
         return "* Message: {}\n* Link: {}\n* Title: {}\n* Comments: {}\n* Likes: {}\n".format(self.message, self.link, self.title, len(self.comments), len(self.likes))
 
+    # a class method to examine a post and generate a list of words that are not stopwords
     def stopwords_remover(self):
         # a list of stopwords
         stopwords_lst = ["the", "a", "and", "is", "are", "that", "i", "my", "when", "which", "this", "of", "to", "will", "for", "you", "your", "from", "so", "it", "all", "she", "her", "he", "his", "we", "us", "our", "in", "me", "on", "must", "by", "have", "own", "out", "be"]
 
-        message_content = self.message.lower().split()
+        message_content = self.message.lower().replace(".", "").replace(",", "").replace("?", "").replace("!", "").split()
         post_without_stopwords = []
         for word in message_content:
             if word not in stopwords_lst:
                 post_without_stopwords.append(word)
-
-        # return a list of all the words in the post that are not “stopwords”
         return post_without_stopwords
+
+# STEP 3. FIND THE MOST COMMON WORD
+facebook_test_data_1 = request_my_facebook_data()
+post_lst = []
+for message_diction in facebook_test_data_1['data']:
+    inst = Post(message_diction)
+    post_lst.append(inst.stopwords_remover())
+
+print(post_lst)
+
+word_counts = {}
+for l in post_lst:
+    for word in l:
+        if word not in word_counts:
+            word_counts[word] = 0
+        word_counts[word] += 1
+
+most_frequent_word = ""
+frequency = 0
+
+for word in word_counts:
+    if word_counts[word] > frequency:
+        frequency = word_counts[word]
+        most_frequent_word = word
+
+print(most_frequent_word)
 
 # ===== iTunes =====
 # PART 1. DATA REQUEST & CACHING
@@ -147,10 +177,3 @@ class Song:
 # for diction in itunes_test_data_1['results']:
 #     inst = Song(diction)
 #     print(inst)
-
-# --- Facebook ---
-facebook_test_data_1 = request_my_facebook_data()
-for message_diction in facebook_test_data_1['data']:
-    inst = Post(message_diction)
-    lst = inst.stopwords_remover()
-    print(lst)
